834b0178a1dabdae4decb0ddf7b60cb9c82638d7
Lucas Skog <lucasskog@gmail.com>
2024-02-25 14:06:30 +0100

    Initial files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./scss/main.scss";
+import { LandingPage } from "./pages/LandingPage";
+
+export function App() {
+  return (
+    <>
+      <h1>This is the App component</h1>
+      <LandingPage/>
+    </>
+  );
+}
---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import pic from "../assets/todo_icon.svg";
+
+export function LandingPage() {
+  return (
+    <div>
+        <button>Add</button>
+        <img src={pic}/>
+    </div>
+  );
+}
+
